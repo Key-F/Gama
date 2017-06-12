9e9c14227968dfd76f5aeeaa0b30f6ed7ab12aad
Key-F <kirill2010.97@mail.ru>
2017-06-13 00:11:45 +0300

    BallControl na Sharpe

---
+++ Assets/scripts/BallControlCSharp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class BallControlCSharp : MonoBehaviour {
+
+    public int speed = 500;
+    public Rigidbody rb;
+
+    void FixedUpdate()
+    {      
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, -speed * Time.deltaTime, 0);
+        }
+    }
+       
+
+       
+}
