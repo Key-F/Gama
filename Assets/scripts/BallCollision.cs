888d3927e2c07ed0d66495e9c0521c89870c08ac
Key-F <kirill2010.97@mail.ru>
2017-07-01 00:27:36 +0300

    монеты исчезают
    todo:
    остановка движения при перезапуске

--- Assets/scripts/BallCollision.cs
+++ Assets/scripts/BallCollision.cs
@@ -3,14 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class BallCollision : MonoBehaviour {
 
+    public Transform ball;
+
     void OnCollisionEnter(Collision collInfo)
     {
         if (collInfo.collider.tag == "EvilWall")
         {
-            Debug.Log("pizda nam");
-            //Application.LoadLevel("Lvl1");           
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            //Debug.Log("pizda nam");
+            //Application.LoadLevel("Lvl1"); 
+            if (BallControlCSharp.hardcore == true)
+            {
+                ball.position = new Vector3(0, 1.34f, 0);
+            }
+            else
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
             GuimMuniger.currentScore -= 1;
+
         }
 
         if (collInfo.collider.name == "EndGame")
@@ -22,7 +32,8 @@ public class BallCollision : MonoBehaviour {
         if (collInfo.collider.tag == "EndLvl")
         {
             Debug.Log("go otsuda");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);           
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);     
+            
         }
 
     }
--- Assets/scripts/BallControlCSharp.cs
+++ Assets/scripts/BallControlCSharp.cs
@@ -5,40 +5,55 @@ public class BallControlCSharp : MonoBehaviour {
     public int speed = 500;
     public int sndspeed = 2000;
     public Rigidbody rb;
-    public bool AddForce = false;
+    public bool AddForce = false; // Тип движения
+    public static  bool hardcore = true; // Монеты не обновляются
+    private bool KeyPressedAtStart = false;
 
+    
     void FixedUpdate()
-    {   
-        
-        if (Input.GetKey("w"))
+    {
+        /*if (Input.anyKey)
         {
-            //AddForce == true ? rb.AddForce(0, sndspeed * Time.deltaTime, 0) : rb.velocity = Vector2.up * speed;
-            if (AddForce)
-                rb.AddForce(0, sndspeed * Time.deltaTime, 0);
-            else
-            rb.velocity = Vector2.up * speed;
+            Debug.Log("A key is being pressed");
+            KeyPressedAtStart = true;
         }
-        if (Input.GetKey("a"))
+        if (KeyPressedAtStart == true)
         {
-            if (AddForce)
-                rb.AddForce(-sndspeed * Time.deltaTime, 0, 0);
-            else
-            rb.velocity = Vector2.left * speed;
+            if (!Input.anyKey)
+                KeyPressedAtStart = false;
         }
-        if (Input.GetKey("d"))
-        {
-            if (AddForce)
-                rb.AddForce(sndspeed * Time.deltaTime, 0, 0);
-            else
-            rb.velocity = Vector2.right * speed;
-        }
-        if (Input.GetKey("s"))
-        {
-            if (AddForce)
-                rb.AddForce(0, -sndspeed * Time.deltaTime, 0);
-            else
-            rb.velocity = Vector2.down * speed;
-        }
+        else 
+        { */
+            if (Input.GetKey("w"))
+            {
+                //AddForce == true ? rb.AddForce(0, sndspeed * Time.deltaTime, 0) : rb.velocity = Vector2.up * speed;
+                if (AddForce)
+                    rb.AddForce(0, sndspeed * Time.deltaTime, 0);
+                else
+                    rb.velocity = Vector2.up * speed;
+            }
+            if (Input.GetKey("a"))
+            {
+                if (AddForce)
+                    rb.AddForce(-sndspeed * Time.deltaTime, 0, 0);
+                else
+                    rb.velocity = Vector2.left * speed;
+            }
+            if (Input.GetKey("d"))
+            {
+                if (AddForce)
+                    rb.AddForce(sndspeed * Time.deltaTime, 0, 0);
+                else
+                    rb.velocity = Vector2.right * speed;
+            }
+            if (Input.GetKey("s"))
+            {
+                if (AddForce)
+                    rb.AddForce(0, -sndspeed * Time.deltaTime, 0);
+                else
+                    rb.velocity = Vector2.down * speed;
+            }
+       // }
     }
        
 
